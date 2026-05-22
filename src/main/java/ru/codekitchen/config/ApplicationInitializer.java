a0e516dbaf2c87233cb765daaa4ecf1116c9cfda
sersdi <sersdi@live.ru>
2026-05-22 20:15:17 +0700

    added ApplicationInitializer.java, WebConfig.java

---
+++ src/main/java/ru/codekitchen/config/ApplicationInitializer.java
@@ -0,0 +1,11 @@
+package ru.codekitchen.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import javax.servlet.ServletContext;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext){
+
+    }
+}
---
+++ src/main/java/ru/codekitchen/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ru.codekitchen.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.codekitchen")
+public class WebConfig implements WebMvcConfigurer {
+}
